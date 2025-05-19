c2c4d256d714395a6ab7a22f3f6c1277959d90d4
taaija <aijamaltabyldieva29@gmail.com>
2025-05-20 01:31:03 +0600

    Обновил проект и настройки деплоя

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import MySQLdb
+import os
 
 def get_connection():
     return MySQLdb.connect(
-        host="localhost",
-        user="root",
-        passwd="tasa2004",
-        db="messenger_db",
+        host=os.getenv('DB_HOST', 'db4free.net'),
+        user=os.getenv('DB_USER', 'sellin29'),
+        passwd=os.getenv('DB_PASSWORD', 'aijamal29062004'),
+        db=os.getenv('DB_NAME', 'messenger_chat'),
         charset='utf8mb4'
     )
